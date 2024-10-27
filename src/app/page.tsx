04b28765a9aff3ef1c1e2ab6630cb3d05239df33
AlanReumy <alanreumy@outlook.com>
2024-10-27 13:18:38 +0800

    feat: use auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,34 @@
 import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function ImageList() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.url} className="w-48" alt="" />
+            <div key={item.id}>{item.name}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((item) => {
-          return (
-            <div key={item.id}>
-              <img src={item.url} className="w-48" alt="" />
-              <div key={item.id}>{item.name}</div>
-            </div>
-          );
-        })}
-      </div>
+      <SignedOut>please sign in above</SignedOut>
+      <SignedIn>
+        <ImageList />
+      </SignedIn>
     </main>
   );
 }
